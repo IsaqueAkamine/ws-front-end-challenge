a8d4a1d34f309f01be7f2fe7cc0c0a0efc717393
Isaque Akamine <isaqueakamine@gmail.com>
2020-03-02 23:59:27 -0300

    Updates

---
+++ precache-manifest.abf010c137896c7d34ada645b03a1663.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "903bb38da1b8467dd7a9ff57325e2ef2",
+    "url": "/ws-front-end-challenge/index.html"
+  },
+  {
+    "revision": "fed82546673d8e123bb3",
+    "url": "/ws-front-end-challenge/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "890d9c060a99145b973c",
+    "url": "/ws-front-end-challenge/static/css/main.c1fd7783.chunk.css"
+  },
+  {
+    "revision": "fed82546673d8e123bb3",
+    "url": "/ws-front-end-challenge/static/js/2.a8cd84ee.chunk.js"
+  },
+  {
+    "revision": "6394823438fea37236d980ffb4f74e4d",
+    "url": "/ws-front-end-challenge/static/js/2.a8cd84ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "890d9c060a99145b973c",
+    "url": "/ws-front-end-challenge/static/js/main.674ee8f0.chunk.js"
+  },
+  {
+    "revision": "c599d67de716bf038d27",
+    "url": "/ws-front-end-challenge/static/js/runtime-main.f08c1245.js"
+  }
+]);
